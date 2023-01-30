d594ccf091a0f41b2e6ee2ed7c1b752d33147df7
Dipjyoti Sikder <dipjyoti.sikder@bjitgroup.com>
2023-01-30 12:29:29 +0600

    Added tribonacci solution

--- TribonacciProblem/Program.cs
+++ TribonacciProblem/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using TribonacciProblem;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new TribonacciProblemsSolution();
+        var result = solution.Tribonacci(25);
+        Console.WriteLine("Result: {0}", result);
+    }
+}
---
+++ TribonacciProblem/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TribonacciProblem
+{
+    public class TribonacciProblemsSolution
+    {
+        public int Tribonacci(int n)
+        {
+            if (n <= 0) return 0;
+
+            var generated = new List<int>
+            {
+                0
+            };
+
+            Console.WriteLine("index {0} total value {1}", 0, 0);
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (generated.Count < 3)
+                {
+                    if (generated.Count == 1)
+                    {
+                        generated.Add(1);
+                        Console.WriteLine("index {0} total value {1}", 1, 1);
+                    }
+                    else if (generated.Count == 2)
+                    {
+                        generated.Add(1);
+                        Console.WriteLine("index {0} total value {1}", 2, 1);
+                    }
+                }
+                else
+                {
+                    var total = 0;
+                    var start = (i - 3);
+                    for (int j = start; j <= i - 1; j++)
+                    {
+                        total = total + generated[j];
+                    }
+                    generated.Add(total);
+                    Console.WriteLine("index {0} total value {1}", i, total);
+                }
+            }
+            return generated[n];
+        }
+    }
+}
